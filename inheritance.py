416e2799cdee5cd2377b585cd31ccb2ced1dfa63
SamanthaCorner <72683419+SamanthaCorner@users.noreply.github.com>
2021-03-07 10:05:28 +0200

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,26 @@
+# notes on inheritance
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("inhale ... exhale")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breathe(self):
+        super().breathe()
+        print("but only underwater")
+
+    def swim(self):
+        print("moving in water")
+
+dog = Animal()
+dog.breathe()
+
+nemo = Fish()
+nemo.breathe()
+nemo.swim()
